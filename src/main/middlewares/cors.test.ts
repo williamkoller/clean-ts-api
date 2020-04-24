c9e9a7001e4211b4cdd2644b23a89d1f03e6e24d
William Koller <wkoller25@gmail.com>
2020-04-23 22:36:38 -0300

    feat: ensure requests return as default content type

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,7 +3,7 @@ import app from '../config/app'
 
 describe('Cors Middleware', () => {
   test('Should enable CORS', async () => {
-    app.post('/test_cors', (req, res) => {
+    app.get('/test_cors', (req, res) => {
       res.send()
     })
     await request(app)
