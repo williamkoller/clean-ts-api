b1525e7b024f65e95e30d924b170e8f604e3f060
William Koller <wkoller25@gmail.com>
2020-04-24 22:40:00 -0300

    chore: add rootDir

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
