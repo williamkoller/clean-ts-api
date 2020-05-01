48dc0521165c2af361560c8b4488a8872bbd93c7
William Koller <wkoller25@gmail.com>
2020-05-01 15:40:34 -0300

    feat: add LoginValidationFactory

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,5 +1,4 @@
 export * from '../../protocols'
-export * from '../../protocols/email-validator'
+export * from '../../helpers/validators/validation'
 export * from '../../../domain/usercases/add-account'
 export * from '../../../domain/models/account'
-export * from '../../helpers/validators/validation'
