17c406217e17d295cffbdbe2c8431c8e6b098815
William Koller <wkoller25@gmail.com>
2020-05-09 17:45:33 -0300

    refactor: ensure DbAccount return an account

--- src/main/factories/decorators/log-controller-decorator-factory.ts
+++ src/main/factories/decorators/log-controller-decorator-factory.ts
@@ -2,7 +2,7 @@ import { LogControllerDecorator } from '../../decorators/log-controller-decorato
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { Controller } from '../../../presentation/protocols'
 
-export const makeLogControllerDecorator = (controller): Controller => {
+export const makeLogControllerDecorator = (controller: Controller): Controller => {
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(controller, logMongoRepository)
 }
