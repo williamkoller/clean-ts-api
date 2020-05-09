46a8bd5aba5e38cdf2c5d8df0711fef9cd46ef99
William Koller <wkoller25@gmail.com>
2020-05-09 18:32:47 -0300

    refactor: add correct commit

--- src/presentation/controllers/signup/signup-controller-protocols.ts
+++ src/presentation/controllers/signup/signup-controller-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../protocols'
-export * from '../../protocols/validation'
 export * from '../../../domain/usercases/add-account'
+export * from '../../../domain/usercases/authentication'
 export * from '../../../domain/models/account'
