479718cdad14f12145f52c352066aab737d004c1
William Koller <wkoller25@gmail.com>
2020-05-10 22:01:51 -0300

    refactor: move login and signup controllers to a folder

---
+++ src/presentation/controllers/signup/signup/signup-controller-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../protocols'
+export * from '../../../../domain/usercases/add-account'
+export * from '../../../../domain/usercases/authentication'
+export * from '../../../../domain/models/account'
