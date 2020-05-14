2ae10d5c96047399f5b62b388b97adcda82c76af
William Koller <wkoller25@gmail.com>
2020-05-13 23:45:31 -0300

    refactor: organize main layer folders

--- src/main/adapters/validators/email-validator-adapter.ts
+++
@@ -1,8 +0,0 @@
-import { EmailValidator } from '../../../validation/protocols/email-validator'
-import validator from 'validator'
-
-export class EmailValidatorAdapter implements EmailValidator {
-  isValid (email: string): boolean {
-    return validator.isEmail(email)
-  }
-}
