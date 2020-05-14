335c8a501ab3e77c0c5674802469b3f533e64d29
William Koller <wkoller25@gmail.com>
2020-05-13 22:18:42 -0300

    test: ensure DbLoadAccountByToken returns an account success

--- src/data/usercases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usercases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -73,4 +73,10 @@ describe('DbLoadAccountByToken UseCase', () => {
     const account = await sut.load('any_token', 'any_role')
     expect(account).toBeNull()
   })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const account = await sut.load('any_token', 'any_role')
+    expect(account).toEqual(makeFakeAccount())
+  })
 })
