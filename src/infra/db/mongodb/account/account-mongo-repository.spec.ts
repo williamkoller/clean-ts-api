374431b6a71c88b06605fc5e0f794cf82a145cde
William Koller <wkoller25@gmail.com>
2020-05-14 00:06:41 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -110,5 +110,11 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email@mail.com')
       expect(account.password).toBe('any_password')
     })
+
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByEmail('any_token')
+      expect(account).toBeFalsy()
+    })
   })
 })
