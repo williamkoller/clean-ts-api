c0576cb0b3105908f28f04b9eb1f0e4328f63436
William Koller <wkoller25@gmail.com>
2020-05-23 18:40:11 -0300

    feat: ensure SaveSurveyResultController calls SaveSurveyResult with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller-protocols.ts
@@ -1,3 +1,5 @@
 export * from '@/presentation/protocols'
-export * from '@/domain/models/survey'
 export * from '@/domain/usercases/survey/load-survey-by-id'
+export * from '@/domain/usercases/survey-result/save-survey-result'
+export * from '@/domain/models/survey'
+export * from '@/domain/models/survey-result'
