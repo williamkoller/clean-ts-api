08eac665879bb9dbe0b4f241f2f6646ce7471ac2
William Koller <wkoller25@gmail.com>
2020-05-26 20:54:29 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,9 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API SignUp para realizar enquetes entre programadores',
+    version: '1.0.0'
+  }
+
+}
