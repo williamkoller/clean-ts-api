afd51ab82d31a7d7b55d91e1ee9549c2c5fd1280
William Koller <wkoller25@gmail.com>
2020-06-02 22:09:05 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -5,9 +5,11 @@ export const surveyResultPath = {
     }],
     tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
     parameters: [{
       in: 'path',
       name: 'surveyId',
+      description: 'ID da enquete a ser respondida',
       required: true,
       schema: {
         type: 'string'
@@ -33,8 +35,43 @@ export const surveyResultPath = {
           }
         }
       },
-      400: {
-        $ref: '#/components/badRequest'
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  },
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    description: 'Essa rota só pode ser executada por **usuários autenticados**',
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      description: 'ID da enquete a ser respondida',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
       },
       403: {
         $ref: '#/components/forbidden'
